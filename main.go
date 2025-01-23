c699493330ac85dc461a77b7bf30e317a44d96a7
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-01-22 17:08:25 -0800

    chore: formatted

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/rhysmah/note-app/cmd"
+import (
+	"github.com/rhysmah/note-app/cmd"
+)
 
 func main() {
 	cmd.Execute()
