2c0af0e8f3f60c6d2dd520289de369df9cfb14f3
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-01-23 17:28:05 -0800

    feature: implemented simple list function

--- cmd/list.go
+++ cmd/list.go
@@ -4,37 +4,49 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/rhysmah/note-app/internal/logger"
 	"github.com/spf13/cobra"
 )
 
-var listLogger *logger.Logger
-
 func init() {
-	rootCmd.AddCommand()
+	rootCmd.AddCommand(list)
 }
 
 var list = &cobra.Command{
 	Use:   "list",
 	Short: "Show all notes",
 	Run: func(cmd *cobra.Command, args []string) {
+		appLogger.Log("[START]: Starting to list notes")
 
+		_, err := dirManager.ConfirmUserHomeDirectory()
+		if err != nil {
+			appLogger.Log(fmt.Sprintf("[ERROR] Home directory operation failed: %v", err))
+			fmt.Println(err)
+			return
+		}
+
+		notesDir, err := dirManager.ConfirmNotesDirectory()
+		if err != nil {
+			appLogger.Log(fmt.Sprintf("[ERROR] Notes directory creation failed: %v", err))
+			fmt.Println(err)
+			return
+		}
+
+		err = listNotes(notesDir)
+		if err != nil {
+			fmt.Println(err)
+		}
 	},
 }
 
-// confirmUserHomeDirectory retrieves the user's home directory path from the operating system.
-// It returns the absolute path to the user's home directory as a string and any error encountered.
-// If the home directory cannot be determined, it returns an empty string and a descriptive error.
-func confirmUserHomeDirectory() (string, error) {
-	appLogger.Log("[START] Looking up user home directory")
-
-	userHomeDir, err := os.UserHomeDir()
+// HELPERS
+func listNotes(notesDir string) error {
+	notes, err := os.ReadDir(notesDir)
 	if err != nil {
-		errMsg := fmt.Sprintf("[ERROR] Home directory lookup failed: %v", err)
-		appLogger.Log(errMsg)
-		return "", fmt.Errorf("%s", errMsg)
+		return err
 	}
 
-	appLogger.Log(fmt.Sprintf("[SUCCESS] Found home directory at: %s", userHomeDir))
-	return userHomeDir, nil
+	for _, note := range notes {
+		fmt.Println(note.Name())
+	}
+	return nil
 }
