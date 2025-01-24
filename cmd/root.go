123475fbb8708e1024b93d5f10ba33879738846a
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-01-23 17:29:37 -0800

    refactor: confirmed user dir, create note dir in rooCmd

--- cmd/root.go
+++ cmd/root.go
@@ -7,8 +7,14 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/rhysmah/note-app/internal/filesystem"
 	"github.com/rhysmah/note-app/internal/logger"
 	"github.com/spf13/cobra"
+)
+
+var (
+	appLogger  *logger.Logger
+	dirManager *filesystem.DirectoryManager
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -22,10 +28,26 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+
 		var err error
 		appLogger, err = logger.New()
 		if err != nil {
 			fmt.Printf("Failed to initialize logger: %v\n", err)
+			os.Exit(1)
+		}
+
+		dirManager = filesystem.NewDirectoryManager(appLogger)
+		_, err = dirManager.ConfirmUserHomeDirectory()
+		if err != nil {
+			appLogger.Log(fmt.Sprintf("[ERROR] Home directory operation failed: %v", err))
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		_, err = dirManager.ConfirmNotesDirectory()
+		if err != nil {
+			appLogger.Log(fmt.Sprintf("[ERROR] Notes directory creation failed: %v", err))
+			fmt.Println(err)
 			os.Exit(1)
 		}
 	},
