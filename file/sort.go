07f845f64abdf09ad4d51b606dec376d731e17b0
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-01-29 18:34:05 -0800

    feature: write sort functions

--- file/sort.go
+++ file/sort.go
@@ -1,1 +1,13 @@
 package file
+
+type ByCreationDate []File
+
+func (bcd ByCreationDate) Len() int { return len(bcd) }
+func (bcd ByCreationDate) Swap(i, j int) { bcd[i], bcd[j] = bcd[j], bcd[i] }
+func (bcd ByCreationDate) Less(i, j int) bool { return bcd[i].DateCreated.Before(bcd[j].DateCreated) }
+
+type ByModifiedDate []File
+
+func (bmd ByModifiedDate) Len() int { return len(bmd) }
+func (bmd ByModifiedDate) Swap(i, j int) { bmd[i], bmd[j] = bmd[j], bmd[i] }
+func (bmd ByModifiedDate) Less(i, j int) bool { return bmd[i].DateCreated.Before(bmd[j].DateCreated) }
