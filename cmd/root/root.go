af7add348cb5ffec5f3b0cee07248fe8d4a36d63
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-08 12:17:44 -0800

    chore: update imports

---
+++ cmd/root/root.go
@@ -0,0 +1,51 @@
+package root
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/rhysmah/note-app/internal/filesystem"
+	"github.com/rhysmah/note-app/internal/logger"
+	"github.com/spf13/cobra"
+)
+
+var (
+	AppLogger  *logger.Logger
+	DirManager *filesystem.DirectoryManager
+)
+
+var RootCmd = &cobra.Command{
+	Use: "note-app",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+
+		var err error
+		AppLogger, err = logger.NewLogger()
+		if err != nil {
+			fmt.Printf("Failed to initialize logger: %v\n", err)
+			os.Exit(1)
+		}
+
+		DirManager, err = filesystem.NewDirectoryManager(AppLogger)
+		if err != nil {
+			fmt.Printf("Home directory operation failed: %v", err)
+			os.Exit(1)
+		}
+	},
+
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		if AppLogger != nil {
+			AppLogger.CloseCurrentLogFile()
+		}
+	},
+}
+
+func Execute() {
+	err := RootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
