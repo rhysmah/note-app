54567e6e066bdf06de8cbe991c792e2d64ca3f6a
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-09 15:55:30 -0800

    refactor: add imports for command init() to work

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/rhysmah/note-app/cmd/root"
+import (
+	_ "github.com/rhysmah/note-app/cmd/list"
+	_ "github.com/rhysmah/note-app/cmd/new"
+	"github.com/rhysmah/note-app/cmd/root"
+)
 
 func main() {
 	root.Execute()
