60d06d9a5777dafa1480f8a4cf666f36a572877b
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-09 15:53:57 -0800

    feature: add sorting

---
+++ cmd/list/sort.go
@@ -0,0 +1,33 @@
+package list
+
+import (
+	"sort"
+
+	"github.com/rhysmah/note-app/file"
+)
+
+func SortFiles(files []file.File, field SortField, order SortOrder) {
+	sort.Slice(files, func(a, b int) bool {
+		return compareFiles(files[a], files[b], field, order)
+	})
+}
+
+func compareFiles(a, b file.File, field SortField, order SortOrder) bool {
+	switch field {
+
+	case SortFieldCreated:
+		if order == SortOrderNewest {
+			return a.DateCreated.After(b.DateCreated)
+		}
+		return a.DateCreated.Before(b.DateCreated)
+
+	case SortFieldModified:
+		if order == SortOrderNewest {
+			return a.DateModified.After(b.DateModified)
+		}
+		return a.DateCreated.Before(b.DateModified)
+
+	default:
+		return a.Name < b.Name
+	}
+}
