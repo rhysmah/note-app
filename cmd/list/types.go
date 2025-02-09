2d22ab095b1de657d6181a79f506256c76b40c34
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-09 15:54:17 -0800

    feature: list-spsecific types

--- cmd/list/types.go
+++ cmd/list/types.go
@@ -2,14 +2,13 @@ package list
 
 import "github.com/rhysmah/note-app/file"
 
-// Example: we may sort the FIELD Date Modified in the ORDER newest to oldest
-type SortField string  // The field used to sort files
-type SortOrder string  // The order to sort the field
+type SortField string
+type SortOrder string
 
 const (
 	SortFieldModified SortField = "modified"
-	SortFieldCreated SortField = "created"
-	SortFieldName SortField = "name"
+	SortFieldCreated  SortField = "created"
+	SortFieldName     SortField = "name"
 )
 
 const (
@@ -18,13 +17,13 @@ const (
 )
 
 // Map SortField types to descriptions
-var sortFieldDescriptions = map[SortField]string {
+var sortFieldDescriptions = map[SortField]string{
 	SortFieldModified: "modification date",
-	SortFieldCreated: "creation date",
-	SortFieldName: "file name",
+	SortFieldCreated:  "creation date",
+	SortFieldName:     "file name",
 }
 
-var sortOrderDescriptions = map[SortOrder]string {
+var sortOrderDescriptions = map[SortOrder]string{
 	SortOrderNewest: "newest to oldest",
 	SortOrderOldest: "oldest to newest",
 }
@@ -32,5 +31,5 @@ var sortOrderDescriptions = map[SortOrder]string {
 type ListOptions struct {
 	SortField SortField
 	SortOrder SortOrder
-	files []file.File
+	files     []file.File
 }
