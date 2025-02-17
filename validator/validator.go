a0f6b4fbccf08f077f4f318eff662255e9574daf
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-17 14:58:40 -0800

    refactor: generic validator

--- validator/validator.go
+++ validator/validator.go
@@ -3,17 +3,17 @@ package validator
 type ValidationRule[T any] func(*T) error
 
 type Validator[T any] struct {
-	rules []ValidationRule[T]
+	Rules []ValidationRule[T]
 }
 
-func NewValidator[T any](rules ...ValidationRule[T]) *Validator[T] {
+func NewValidator[T any]() *Validator[T] {
 	return &Validator[T]{
-		rules: rules,
+		Rules: []ValidationRule[T]{},
 	}
 }
 
 func (v *Validator[T]) Run(opts *T) error {
-	for _, rule := range v.rules {
+	for _, rule := range v.Rules {
 		if err := rule(opts); err != nil {
 			return err
 		}
